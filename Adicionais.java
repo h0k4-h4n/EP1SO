a5abd53aa1e826e727a2940b84f60aac1839d71a
Raphael Janofsky Pivato <rpivato@server01.foursys.local>
2019-10-02 12:43:28 -0300

    Remoção dos atributos, estes foram todos passados para a classe Gerenciador.java - além deses, os métodos usados para contrução dos buffers tiveram o mesmo destino

--- Adicionais.java
+++ Adicionais.java
@@ -5,70 +5,7 @@ import java.util.LinkedList;
 
 public class Adicionais {
 	
-/* -------- ATRIBUTOS ----------- */
-	
-	// Tabela de Processos (Array ou Lista de BCPs, contendo todos os processos carregados - sem ordenação específica)
-	private LinkedList<BCP> tabelaDeProcessos;
-	
-	// Lista dos processos prontos (Lista de listas de BCPs - ordenação externa por créditos e interna por ordem alfabética)
-	private LinkedList<LinkedList<BCP>> listaDeProntos;
-	
-	// Lista dos processos bloqueados (Array ou Lista de BCPs) - ordenada por ordem de chegada
-	private LinkedList<BCP> listaDeBloqueados;
-	
-	// N_COM - número de comandos que o processador utilizará como parâmetro base - obtido via arquivo quantum.txt
-	private int N_COM;
-	
 /* ---------- MÉTODOS ------------ */
-	
-	// Constrói um buffer com o código do programa, para que haja o controle realizado pelo PC 
-	
-	public String[] bufferizaPrograma(BufferedReader arquivoPrograma, int contador) {
-		String[] buffer = new String[contador];
-		
-		try {
-			String linha = arquivoPrograma.readLine();
-		
-			for (int i = 0; linha != null; i++){
-				buffer[i] = linha;
-				linha = arquivoPrograma.readLine();
-			}
-		}
-		catch(FileNotFoundException e) {
-			System.out.println("Arquivo: " + arquivoPrograma.toString() + " não encontrado");
-			return null;
-		}
-		catch(IOException e) {
-			System.out.println("Erro " + e.toString() + " na leitura do arquivo: " + arquivoPrograma.toString());
-			return null;
-		}
-		
-		return buffer;
-	}
-	
-	// Realiza a contagem de linhas do arquivo programa
-	
-	public int contaPrograma(BufferedReader arquivoPrograma) {
-		int contador = 0;
-		
-		try {
-			String linha = arquivoPrograma.readLine();
-		
-			while(linha != null){
-				contador++;
-				linha = arquivoPrograma.readLine();
-			}
-		}
-		catch(FileNotFoundException e) {
-			System.out.println("Arquivo: " + arquivoPrograma.toString() + " não encontrado");
-			return -1;
-		}
-		catch(IOException e) {
-			System.out.println("Erro " + e.toString() + " na leitura do arquivo: " + arquivoPrograma.toString());
-			return -1;
-		}
-		return contador;
-	}
 	
 	// Interpreta uma linha de código recebida e - no lugar dos printlns - aplicará ou invocará classes/métodos que executem as devidas funções
 	
@@ -89,36 +26,5 @@ public class Adicionais {
 		
 		else
 			System.out.println("Atualizar o registrador Y para: " + (char)linhaFatorada[2]);
-	}
-	
-	
-	//MAIN DE EXEMPLO, EXTRAÍDO DA CLASSE TESTE COM EXEMPLO DE USABILIDADE DO BUFFER
-	public static void main(String[] args) throws FileNotFoundException, IOException, IllegalArgumentException{
-		BufferedReader in = new BufferedReader(new FileReader("01.txt"));
-		in.mark(1000);
-		
-		String[] codigo;
-		int qtdLinhas = contaPrograma(in);
-		
-		if (qtdLinhas > 0){
-			in.reset();
-			codigo = bufferizaPrograma(in, qtdLinhas);
-			for (String linha : codigo)
-				System.out.println(linha);
-		}
-		
-		System.out.println("Quantidade: " + qtdLinhas);
-		
-		in.close();
-	}
-	
-	// Getter e Setters dos atributos
-	
-	public int getN_COM() {
-		return this.N_COM;
-	}
-	
-	public void setN_COM(int N_COM) {
-		this.N_COM = N_COM;
 	}
 }

0e7774915f3f3098bde72f79f936b4b3781445ef
Otto <ottoalves22@gmail.com>
2019-10-26 20:35:42 -0300

    executador

--- Escalonador.java
+++ Escalonador.java
@@ -6,20 +6,34 @@ public class Escalonador {
 
 	/* ------------- ATRIBUTOS ------------- */
 
-	private static int N_COM; 								// Numero de Comandos que cada processo tem direito de executar nas
+	public static int N_COM; 								// Numero de Comandos que cada processo tem direito de executar nas
 													// condicoes iniciais, obtido pelo arquivo quantum.txt
-	private static LinkedList<BCP> tabelaProcessos; 		// Tabela de Processos (Lista de BCPs, contendo todos os processos
+	public static LinkedList<BCP> tabelaProcessos; 		// Tabela de Processos (Lista de BCPs, contendo todos os processos
 													// carregados - sem ordenacao especifica)
-	private static Collection<BCP> listaProntos; 			// Lista dos processos prontos (Lista de BCPs - ordenacao primaria por
+	public static Collection<BCP> listaProntos; 			// Lista dos processos prontos (Lista de BCPs - ordenacao primaria por
 													// creditos e secundaria por ordem alfabetica)
-	private static LinkedList<BCP> listaBloqueados; 		// Lista dos processos bloqueados (Lista de BCPs) - ordenada por
+	public static LinkedList<BCP> listaBloqueados; 		// Lista dos processos bloqueados (Lista de BCPs) - ordenada por
 													// ordem
 													// de chegada
-	private static LogFile logFile;						// Instancia de LogFile
+	public static LogFile logFile;						// Instancia de LogFile
 
 	/* ------------ CONSTRUTORES -------------- */
 
 	public Escalonador() {
+	}
+	
+	/* ------------ GETTERS -------------- */
+
+	public static LinkedList<BCP> getProcessos() {
+		return tabelaProcessos;
+	}
+
+	public static Collection<BCP> getProntos() {
+		return listaProntos;
+	}
+
+	public static LinkedList<BCP> getBloqueados() {
+		return listaBloqueados;
 	}
 
 	/* ---------- METODOS -------------- */
@@ -388,78 +402,5 @@ public class Escalonador {
 		
 		return listaReordenada;
 	}
-
-	public static void executa() {
-		// Declara um BCP generico para realizar as chamadas, um contador de instrucoes
-		// para determinar quantas instrucoes
-		// cada processo realizou e chama a rotina de carregamento dos processos
-		try {
-			carregaProcessos();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		BCP bcp = null;
-		int contaInstrucoes;
-		double contaTrocas = 0;
-		double acumuladorInstrucoes = 0;
-		double acumuladorQuantum = 0;		// Soma o quantum (N_COM * quantum) a cada troca de processos
-
-		// O escalonador executara os processos ate que todos tenham terminado. Para tal
-		// o contador de instrucoes
-		// inicia cada quantum com zero, obtem-se o BCP do processo de maior prioridade
-		// (dada a implementacao via TreeSet,
-		// esse processo estara por ultimo) e o status do mesmo e alterado para E -
-		// Executando
-		while (tabelaProcessos.size() > 0) {
-			contaInstrucoes = 0;
-			boolean finalizado = false;
-			if(listaProntos.isEmpty()==false) {
-				bcp = ((TreeSet<BCP>) listaProntos).last();
-				bcp.setStatusProcesso('E');
-				logFile.msgExecutaProcesso(bcp.getNomePrograma());
-				acumuladorQuantum += (N_COM * bcp.getQuantum());
-	
-				// Executa instrucoes enquanto estiver com status E - Executando ou enquanto a
-				// contagem de instrucoes nao superar
-				// o numero de comandos por quantum multiplicado pela quantidade de quantum que
-				// o processo detem
-				while ((contaInstrucoes < bcp.getQuantum() * N_COM) && (bcp.getStatusProcesso() == 'E')
-						&& finalizado == false) {
-					contaInstrucoes++;
-					acumuladorInstrucoes++;
-					finalizado = interpretaCodigo(bcp, contaInstrucoes);
-					bcp.setPC(bcp.getPC() + 1);
-				}
-	
-				if (finalizado == false) {
-					logFile.msgInterrompeProcesso(bcp.getNomePrograma(), contaInstrucoes);
-					bcp.setQuantum(bcp.getQuantum()+1);
-					bcp.setCreditos(bcp.getCreditos() - 2);
-					if (bcp.getCreditos() < 0) {
-						bcp.setCreditos(0);
-					}
-					if(listaBloqueados.size()>0) {
-						decrementaTempBloqueados();
-					}
-					if(creditoNulo()){
-						restituiCreditos();
-					}
-					contaTrocas++;
-					listaProntos = reordena();
-				}
-			}
-			else {
-				decrementaTempBloqueados();
-			}
-			//imprimeTabelaProcessos();
-			//imprimeListaProntos();
-		}
-		double mediaTrocas = contaTrocas/10;
-		double mediaInstrucoes = acumuladorInstrucoes/acumuladorQuantum;
-		logFile.msgEstatisticas(mediaTrocas, mediaInstrucoes, N_COM);
-
-	}
-
 	
 }
--- Executador.java
+++ Executador.java
@@ -1,9 +1,77 @@
 public class Executador {
 /* --------------- MAIN --------------- */
 
-    public static void main(String[] args) {
-        Escalonador escalonador = new Escalonador();
+public static void main(String[] args) {
 
-        escalonador.executa();
+    Escalonador escalonador = new Escalonador();
+    // Declara um BCP generico para realizar as chamadas, um contador de instrucoes
+    // para determinar quantas instrucoes
+    // cada processo realizou e chama a rotina de carregamento dos processos
+    try {
+        escalonador.carregaProcessos();
+    } catch (Exception e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
     }
+    BCP bcp = null;
+    int contaInstrucoes;
+    double contaTrocas = 0;
+    double acumuladorInstrucoes = 0;
+    double acumuladorQuantum = 0;		// Soma o quantum (N_COM * quantum) a cada troca de processos
+
+    // O escalonador executara os processos ate que todos tenham terminado. Para tal
+    // o contador de instrucoes
+    // inicia cada quantum com zero, obtem-se o BCP do processo de maior prioridade
+    // (dada a implementacao via TreeSet,
+    // esse processo estara por ultimo) e o status do mesmo e alterado para E -
+    // Executando
+    while (escalonador.getProcessos().size() > 0) {
+        contaInstrucoes = 0;
+        boolean finalizado = false;
+        if(escalonador.getProntos().isEmpty()==false) {
+            bcp = ((TreeSet<BCP>) escalonador.listaProntos).last();
+            bcp.setStatusProcesso('E');
+            escalonador.logFile.msgExecutaProcesso(bcp.getNomePrograma());
+            acumuladorQuantum += (escalonador.N_COM * bcp.getQuantum());
+
+            // Executa instrucoes enquanto estiver com status E - Executando ou enquanto a
+            // contagem de instrucoes nao superar
+            // o numero de comandos por quantum multiplicado pela quantidade de quantum que
+            // o processo detem
+            while ((contaInstrucoes < bcp.getQuantum() * escalonador.N_COM) && (bcp.getStatusProcesso() == 'E')
+                    && finalizado == false) {
+                contaInstrucoes++;
+                acumuladorInstrucoes++;
+                finalizado = escalonador.interpretaCodigo(bcp, contaInstrucoes);
+                bcp.setPC(bcp.getPC() + 1);
+            }
+
+            if (finalizado == false) {
+                escalonador.logFile.msgInterrompeProcesso(bcp.getNomePrograma(), contaInstrucoes);
+                bcp.setQuantum(bcp.getQuantum()+1);
+                bcp.setCreditos(bcp.getCreditos() - 2);
+                if (bcp.getCreditos() < 0) {
+                    bcp.setCreditos(0);
+                }
+                if(escalonador.getBloqueados().size()>0) {
+                    escalonador.decrementaTempBloqueados();
+                }
+                if(escalonador.creditoNulo()){
+                    escalonador.restituiCreditos();
+                }
+                contaTrocas++;
+                escalonador.listaProntos = escalonador.reordena();
+            }
+        }
+        else {
+            escalonador.decrementaTempBloqueados();
+        }
+        //imprimeTabelaProcessos();
+        //imprimeListaProntos();
+    }
+    double mediaTrocas = contaTrocas/10;
+    double mediaInstrucoes = acumuladorInstrucoes/acumuladorQuantum;
+    escalonador.logFile.msgEstatisticas(mediaTrocas, mediaInstrucoes, escalonador.N_COM);
+
+}
 }
